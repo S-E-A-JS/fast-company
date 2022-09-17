09c87964acee52fe471d36d2ba9f9cf117f542f1
S-E-A-JS <9168441394@bk.ru>
2022-09-17 19:32:25 +0300

    реализован столбец Избранное, смена состояния bookmark в зависимости от значения поля bookmark в fakeapi

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,35 +1,36 @@
 import React, {useState} from "react";
-import api from "../api";
+import api from "../api"
 
 const Users = () => {
     const [users, setUsers] = useState(api.users.fetchAll());
+
     const handleDelete = (userId) => {
-        setUsers(users.filter((user) => user._id !== userId)); // новый массив юзеров за исключением выбранного по клику на кнопку
-    }
+        setUsers(users.filter((user) => user._id !== userId));
+    };
     const renderPhrase = (number) => {
-        const lastItem = Number(number.toString().slice(-1));
-        const ifLastNumbers = [2, 3, 4];
-        if (lastItem === 1) {
-            return 'человек тусанет';
-        } else if (number > 4 && number < 15) { // при числах между 4 и 15
-            return 'человек тусанет';
-        } else if (ifLastNumbers.indexOf(lastItem) >= 0) { // при числах > 0, если окончание числа 2||3||4
-            return 'человека тусанут';
-        } else { // при 20 человек
-            return 'человек тусанет'
-        }
+        const lastOne = Number(number.toString().slice(-1));
+        if (number > 4 && number < 15) return "человек тусанет";
+        if ([2, 3, 4].indexOf(lastOne) >= 0) return "человека тусанут";
+        if (lastOne === 1) return "человек тусанет";
+        return "человек тусанет";
+    };
+    const handleSwitchBookmarkStatus = (userId) => {
+        const userBookmarkStatus = users.map(user => user._id === userId ? {...user, bookmark: !user.bookmark} : user)
+        setUsers(userBookmarkStatus)
     }
+
     return (
         <>
-            <h1>
-                {/*панель с фразой*/}
-                <span className={'badge ' + (users.length === 0 ? 'bg-danger' : 'bg-primary')}>
+            <h2>
+                <span
+                    className={"badge " + (users.length > 0 ? "bg-primary" : "bg-danger")}
+                >
                     {users.length > 0
-                        ? `${users.length} ${renderPhrase(users.length)} с тобой сегодня`
-                        : 'Никто не тусанет с тобой сегодня'
-                    }
+                        ? `${users.length + " " + renderPhrase(users.length)} с тобой сегодня`
+                        : "Никто с тобой не тусанет"}
                 </span>
-            </h1>
+            </h2>
+
             {users.length > 0 && (
                 <table className="table">
                     <thead>
@@ -37,39 +38,54 @@ const Users = () => {
                         <th scope="col">Имя</th>
                         <th scope="col">Качества</th>
                         <th scope="col">Профессия</th>
-                        <th scope="col">Встретился раз</th>
+                        <th scope="col">Встретился, раз</th>
                         <th scope="col">Оценка</th>
+                        <th scope="col">Избранное</th>
+                        <th/>
                     </tr>
                     </thead>
                     <tbody>
                     {users.map((user) => (
                         <tr key={user._id}>
                             <td>{user.name}</td>
-                            <td key={user.qualities._id}>
-                                {user.qualities.map((i) => (
-                                    <span key={i._id} className={'badge m-1 bg-' + i.color}>
-                            {i.name}
-                        </span>
+                            <td>
+                                {user.qualities.map((item) => (
+                                    <span className={"badge m-1 bg-" + item.color} key={item._id}>
+                                        {item.name}
+                                    </span>
                                 ))}
                             </td>
                             <td>{user.profession.name}</td>
                             <td>{user.completedMeetings}</td>
-                            <td>{user.rate}</td>
+                            <td>{user.rate} /5</td>
+                            <td>{user.bookmark === false
+                                ? (<i
+                                    className="bi bi-bookmark"
+                                    onClick={() => handleSwitchBookmarkStatus(user._id)}
+                                ></i>)
+                                : (
+                                    <i
+                                        className="bi bi-bookmark-check"
+                                        onClick={() => handleSwitchBookmarkStatus(user._id)}
+                                    ></i>
+                                )
+                            }
+                            </td>
                             <td>
-                                <button className={'btn btn-primary bg-danger'} onClick={
-                                    () => handleDelete(user._id)}>
-                                    Delete
+                                <button
+                                    onClick={() => handleDelete(user._id)}
+                                    className="btn btn-danger"
+                                >
+                                    delete
                                 </button>
                             </td>
                         </tr>
                     ))}
                     </tbody>
                 </table>
-            )
-            }
+            )}
         </>
-    )
+    );
 };
 
 export default Users;
-
