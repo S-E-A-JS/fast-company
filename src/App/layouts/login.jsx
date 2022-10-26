dc10f79c81b9a94659b58baa369a9104f3a965fd
S-E-A-JS <9168441394@bk.ru>
2022-10-26 20:57:10 +0600

    create sign in form + validation on empty fields

--- src/App/layouts/login.jsx
+++ src/App/layouts/login.jsx
@@ -1,5 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const Login = () => <h1>Login</h1>
+import TextField from '../components/textField'
+import { validator } from '../utils/validator'
+
+const Login = () => {
+  const [ data, setData ] = useState ( { email: '', password: '' } )
+  const [ errors, setErrors ] = useState ( {} )
+  const handleChange = ( { target } ) => {
+    setData ( prevState => ( {
+      ...prevState,
+      [target.name]: target.value,
+    } ) )
+  }
+
+  const validatorConfig = {
+    email: { isRequired: { message: 'Электронная почта обязательная для заполнения' } },
+    password: { isRequired: { message: 'Пароль обязателен для заполнения' } },
+  }
+
+  useEffect ( () => {
+    validate ()
+  }, [ data ] )
+
+  const validate = () => {
+    const errors = validator ( data, validatorConfig )
+    setErrors ( errors )
+    return Object.keys ( errors ).length === 0
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault ()
+    const isValid = validate ()
+    if ( !isValid ) return
+    console.log ( data )
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextField label='Электронная почта'
+        name='email'
+        value={data.email}
+        onChange={handleChange}
+        error={errors.email}/>
+      <TextField label='Пароль'
+        type='password'
+        name='password'
+        value={data.password}
+        onChange={handleChange}
+        error={errors.password}/>
+      <button type='submit'>Submit</button>
+    </form>
+  )
+}
 
 export default Login
